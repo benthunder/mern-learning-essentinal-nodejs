307ea53175d755aaed14aac6d7b2111f537328cd
BenThunder <ledinthang102@gmail.com>
2020-03-25 13:03:31 +0700

    Standard input output

--- question.js
+++ question.js
@@ -5,13 +5,24 @@ const question = [
 ];
 
 const ask = (i = 0) => {
-    process.stdout.write(`\n\n\n ${question[i]}`);
+    process.stdout.write(`\n\n\n ${question[i]} > `);
 }
 
 ask();
 
+const answer = [];
 
-process.stdin.on('data',data => {
-    process.stdout.write("\n\n"+data.toString().trim()+">\n\n");
-    process.exit();
+process.stdin.on('data', data => {
+    answer.push(data.toString().trim());
+    if (answer.length < question.length) {
+        ask(answer.length);
+    } else {
+        process.exit();
+    }
+});
+
+
+process.on("exit", () => {
+    const [name, activity, lang] = answer;
+    console.log(` ${name} is ${activity} ${lang} `);
 });
